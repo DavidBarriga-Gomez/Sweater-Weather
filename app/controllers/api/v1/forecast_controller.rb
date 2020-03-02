02cfb33abec93b27cc1fae610df5a475112b949a
David Gomez <davdg25@gmail.com>
2020-03-02 12:03:48 -0700

    google request working

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    conn = Faraday.get(url: "https://maps.googleapis.com/maps/api/geocode/json?address=denver,co+CA&key=AIzaSyBp0wYB9jCN3id6w635BT_FkB0NjuHjygc")
-      # faraday.headers['key'] = ENV['google_api']
-    ('  # cant make connection ')
+      # response = Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCFSolOM7Xc7EMazBudkM4EjpVZJEg_zZ8")
+      connection = Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json") do |faraday|
+      faraday.params['key'] = ENV['google_api']
+    end
+    response = connection.get("?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA")
       binding.pry
-    # end
   end
 end
