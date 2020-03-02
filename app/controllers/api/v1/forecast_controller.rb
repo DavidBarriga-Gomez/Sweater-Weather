fdb07349ebd288f21b77edc929fd233a1b858f2c
David Gomez <davdg25@gmail.com>
2020-03-02 11:40:53 -0700

    can't make a real connection

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ForecastController < ApplicationController
   def index
     conn = Faraday.get(url: "https://maps.googleapis.com/maps/api/geocode/json?address=denver,co+CA&key=AIzaSyBp0wYB9jCN3id6w635BT_FkB0NjuHjygc")
       # faraday.headers['key'] = ENV['google_api']
+    ('  # cant make connection ')
       binding.pry
     # end
   end
