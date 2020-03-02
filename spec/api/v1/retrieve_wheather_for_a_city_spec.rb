13c1a994b9806390baa43d7018bb39b83aa91359
David Gomez <davdg25@gmail.com>
2020-03-02 11:10:14 -0700

    call not working for geocode api

--- spec/api/v1/retrieve_wheather_for_a_city_spec.rb
+++ spec/api/v1/retrieve_wheather_for_a_city_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User can', type: :feature do
   it 'retrieve wheather for a city' do
-    visit('/api/v1/forecast')
+    visit("/api/v1/forecast")
 
 
     expect(page).to have_content('denver')
