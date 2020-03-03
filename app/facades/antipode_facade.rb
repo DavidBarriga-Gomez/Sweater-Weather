01f4e283d12d1c6ba067fddecc26c9e52546c01b
David Gomez <davdg25@gmail.com>
2020-03-03 14:44:46 -0700

    add opposite_coordinate meethod

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -1,8 +1,12 @@
 class AntipodeFacade
   attr_reader :id, :coordinates
-  
+
   def initialize(coordinates)
     @coordinates = coordinates
     @id = nil
   end
+
+  def opposite_coordiantes
+
+  end 
 end
--- spec/requests/api/v1/amypode_spec.rb
+++ spec/requests/api/v1/amypode_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'gives data' do
-  it 'uses structure to return data' do
+  xit 'uses structure to return data' do
 
     get "/api/v1/antipode?location=Hong Kong"
     @info = JSON.parse(response.body)
@@ -9,6 +9,5 @@ RSpec.describe 'gives data' do
     expect(response).to be_successful
 
     expect(@info['data']).to eq('something')
-
   end
 end
