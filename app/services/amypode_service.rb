88d4e4b6c44eda2e6886c57c5eda2c8eb5a9a257
David Gomez <davdg25@gmail.com>
2020-03-03 14:03:06 -0700

    amypode returning opposite coordinates

--- app/services/amypode_service.rb
+++ app/services/amypode_service.rb
@@ -5,13 +5,13 @@ class AmypodeService
 
   def connection
     conn = Faraday.new(url: "https://amypode.herokuapp.com") do |faraday|
-      faraday.headers["api_key"] = "amypode_api"
+      # faraday.headers["api_key"] = "amypode_api"
     end
-    response = conn.get("/api/v1/antipodes?#{@original_coordinates}")
-    binding.pry
+    response = conn.get("/api/v1/antipodes?#{@original_coordinates}#{ENV["amypode_api"]}")
+    json = JSON.parse(response.body, symbolize_names: true)
   end
 
   def opposite_coordiantes
-
+    connection[:data][:attributes].to_s.delete(":{}lat=>ong' ''")
   end
 end
--- spec/services/amypode_service_spec.rb
+++ spec/services/amypode_service_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'amypode api consumption' do
   it 'does the thing' do
+
+    amypode_response = File.read('spec/fixtures/amypode_response.json')
+    stub_request(:get, "https://amypode.herokuapp.com/api/v1/antipodes?lat=27&long=-82#{ENV["amypode_api"]}").
+    to_return(status: 200, body: amypode_response)
+
     service = AmypodeService.new("lat=27&long=-82")
 
-    # expect(service.opposite_coordiantes).to eq('something')
-    expect(service.connection).to eq('something')
+    expect(service.opposite_coordiantes).to eq("-27.0,98.0")
   end
 end
