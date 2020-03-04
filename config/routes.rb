2b93f7add09c8e83a20b5ecc14a2bde9ba52c3f7
David Gomez <davdg25@gmail.com>
2020-03-04 09:04:18 -0700

    update files via bundle etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      get '/forecast',    to: 'forecasts#index'
+      get '/backgrounds', to: 'backgrounds#index'
+      post '/users',      to: 'users#create'
+      post '/sessions',   to: 'sessions#create'
+      post '/road_trip',  to: 'road_trips#index'
+    end
+  end
 end
