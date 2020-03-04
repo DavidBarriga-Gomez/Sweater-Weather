335ef607ed46dbd9c7a238165d248d67c75cc345
David Gomez <davdg25@gmail.com>
2020-03-04 09:06:01 -0700

    Sessions controller to handle user functionality

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user &. authenticate(params[:password])
+      render json: UserSerializer.new(user), status: 200
+      # converts user to json else returns error
+    else
+      render json: bad_credentials, status: 400
+    end
+  end
+
+  def bad_credentials
+    {errors:
+    [{
+      "status": 400,
+      "detail": "Bad Credentials"
+    }]}
+  end
+end
